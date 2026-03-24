c39d42f1af1de432b2a53993ac1652d4358be6ce
equiroz1995 <elizabethgaete1995@gmail.com>
2026-03-24 15:28:19 -0300

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    use_azuread_auth     = true
-    use_cli              = true
-    resource_group_name  = "rg-tfstate-poc-01"
-    storage_account_name = "sttfstatepoc1215003353"
-    container_name       = "tfstate"
-  }
-}
