12bc3b32e3e1e11b440725931a3e28ec56790b88
equiroz1995 <elizabethgaete1995@gmail.com>
2026-03-24 15:22:36 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,8 @@
 terraform {
   backend "azurerm" {
+    use_azuread_auth     = true
+    use_cli              = true
+    container_name       = "tfstate"
+    storage_account_name = "sttfstatepoc1215003353"
   }
 }
